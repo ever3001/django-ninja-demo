1e34dc251350bbaf7085bdcefa2ba526bf85c775
Dušan Maďar <madar.dusan@gmail.com>
2023-04-10 17:54:34 +0200

    Split settings.py to multiple modules

--- src/myapp/settings/base.py
+++ src/myapp/settings/base.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z3x8*3fdc1e5ef4314p0vx91fik4k9#!o_=yw^*1)5e9!yt)mz"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "myapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("DB_HOST", "db"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("DB_NAME", "my-app-dev"),
+        "USER": os.getenv("DB_USER", "user-dev"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "passw0rd-dev"),
     }
 }
 
---
+++ src/myapp/settings/local.py
@@ -0,0 +1,5 @@
+from myapp.settings.base import *  # noqa
+
+DEBUG = True
+INSTALLED_APPS += ["django_extensions"]  # noqa
+INTERNAL_IPS = ["127.0.0.1"]
